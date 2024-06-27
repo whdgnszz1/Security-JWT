07df3cad811bc545c2cfeba782408874d324b345
홍종훈 <whdgnszz1@snu.ac.kr>
2024-06-27 13:24:19 +0900

    토큰을 이용한 유저 정보 확인 API

--- src/main/java/com/example/securityjwttemplate/member/MemberController.java
+++ src/main/java/com/example/securityjwttemplate/member/MemberController.java
@@ -2,9 +2,11 @@ package com.example.securityjwttemplate.member;
 
 import com.example.securityjwttemplate.member.request.LoginRequest;
 import com.example.securityjwttemplate.member.request.SignupRequest;
+import com.example.securityjwttemplate.security.UserDetailsImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +25,11 @@ public class MemberController {
     public ResponseEntity<String> login(@RequestBody LoginRequest request) {
         String token = service.login(request.getUsername(), request.getPassword());
         return ResponseEntity.ok(token);
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<MemberEntity> getUserInfo(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        MemberEntity member = userDetails.getMember();
+        return ResponseEntity.ok(member);
     }
 }
--- src/main/java/com/example/securityjwttemplate/security/JwtAuthenticationFilter.java
+++ src/main/java/com/example/securityjwttemplate/security/JwtAuthenticationFilter.java
@@ -21,7 +21,7 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
     public JwtAuthenticationFilter(JwtUtil jwtUtil) {
         this.jwtUtil = jwtUtil;
-        setFilterProcessesUrl("/api/user/login");
+        setFilterProcessesUrl("/api/v1/user/login");
     }
 
     @Override
