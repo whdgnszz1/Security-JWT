1fa90a0db112be8241db6c2992881d318a72bb0b
홍종훈 <whdgnszz1@snu.ac.kr>
2024-06-28 05:05:47 +0900

    Access Token을 Cookie에 추가하여 응답

--- src/main/java/com/example/securityjwttemplate/member/MemberController.java
+++ src/main/java/com/example/securityjwttemplate/member/MemberController.java
@@ -5,6 +5,8 @@ import com.example.securityjwttemplate.member.domain.MemberEntity;
 import com.example.securityjwttemplate.member.request.LoginRequest;
 import com.example.securityjwttemplate.member.request.SignupRequest;
 import com.example.securityjwttemplate.security.UserDetailsImpl;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,6 +15,7 @@ import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Base64;
 import java.util.Map;
 
 @RestController
@@ -28,10 +31,23 @@ public class MemberController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody LoginRequest request, HttpServletResponse response) {
         Map<String, String> tokens = service.login(request.getUsername(), request.getPassword());
+
+        // 액세스 토큰 쿠키 설정
+        Cookie accessTokenCookie = new Cookie("accessToken", Base64.getUrlEncoder().encodeToString(tokens.get("accessToken").getBytes()));
+        accessTokenCookie.setHttpOnly(true);
+        accessTokenCookie.setSecure(true); // HTTPS에서만 전송되도록 설정
+        accessTokenCookie.setPath("/");
+        accessTokenCookie.setMaxAge(60 * 60); // 1시간 유효
+
+        // 응답에 쿠키 추가
+        response.addCookie(accessTokenCookie);
+
+        // 응답 헤더에 액세스 토큰 추가
         HttpHeaders headers = new HttpHeaders();
         headers.add(JwtUtil.AUTHORIZATION_HEADER, tokens.get("accessToken"));
+
         return ResponseEntity.ok().headers(headers).body(tokens);
     }
 
